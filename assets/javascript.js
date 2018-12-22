b6dbe49826475061fcc6e2f9e6f90e737dee36c6
Sarah Kinneer <kinneers@gmail.com>
2018-12-21 21:44:52 -0500

    Working- ready for css styling

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
     var topics = ["Brad Pitt", "Angelina Jolie", "Jennifer Lawrence", "Tom Hanks", "Robert De Niro", "Johnny Depp", "Al Pacino", "Denzel Washington", "Russell Crowe", "Leonardo DiCaprio", "Tom Cruise", "John Travolta", "Arnold Schwarzenegger", "Kate Winslet", "Christian Bale", "Morgan Freeman", "Keanu Reeves", "Nicolas Cage", "Hugh Jackman", "Bruce Willis", "Will Smith", "Keira Knightly", "Vin Diesel", "Matt Damon", "Catherine Zeta-Jones", "George Clooney", "Scarlett Johansson", "Robert Downey, Jr.", "Sandra Bullock", "Meg Ryan", "Megan Fox", "Nicole Kidman", "Cameron Diaz", "Katherine Heigl"];
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=HauhqwQL2R2AM9YsD534mHau5NQBTYe7&limit=10";
     var btns = 0;
-    //I need some kind of search like: var search = prompt("enter a search for GIFs");
+    var selection='';
+
+    $("#select-topic").on('click', function(event) {
+        event.preventDefault();
+        selection = $('#topic-input').val().trim();
+        topics.push(selection);
+        $('#topic-input').val('');
+        $(".buttons").empty();
+      
+        //Checks that there is text in the form field
+        for(var i = 0; i < topics.length; i++){
+            renderButtons(topics[i]);
+            btns++;
+        }
+        if (!selection) {
+            $('#topic-input').after("Please enter a name to continue.");
+            return false;
+        }
+        else {
+            console.log(selection);
+          
+        }
+        console.log(topics);
+    })
+
 
     function renderButtons(topics) {
         var newButton = $("<input>").attr({
@@ -10,30 +33,62 @@ $(document).ready(function() {
         value: topics,
         id: "btn" + btns
         });
-        $("body").append(newButton);
+        newButton.addClass('topic')
+        $(".buttons").append(newButton);
     }
-
     for(var i = 0; i < topics.length; i++){
         renderButtons(topics[i]);
         btns++;
     }
+  
 
+    $(document).on('click', '.topic', function(){
+        var topic = $(this).attr('value');
+        $.ajax({
+            url: "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=HauhqwQL2R2AM9YsD534mHau5NQBTYe7&limit=10",
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            $('.grid').html('<h2>Click an Image to Animate the GIF');
+            displayImages(response);
+        }) 
+    })
+
+/*
+    $("button").on("click", function() {
+        var choice = $(this).val();
+  
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=HauhqwQL2R2AM9YsD534mHau5NQBTYe7&limit=10";
+*/
     //Change this to display the button clicked on
     function displayImages(response) {
         response.data.forEach(function(image) {
-            var newDiv = $("body").append("<div>");
-            var newImage = $("<img>").attr("src", image.images.fixed_height_downsampled.url);
-            $(newDiv).append(newImage);          
-        });
+            var newDiv = $("<div>");
+            
+            //Both images work, but I need to trad the data attribute after appending to the page
+            var newImage = $("<img>").attr("class", "gif grid-item").attr("data-state", "still").attr("src", image.images.fixed_width_still.url).attr("data-animate", image.images.fixed_width.url).attr("data-still", image.images.fixed_width_still.url);
+            newDiv.append(newImage);
+            $('.grid').append(newDiv);
+           
+        });   
+        
+        $(".gif").on("click", function() {
+            var state = $(this).attr("data-state");
+            console.log(state);
+      
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            }
+      
+            else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+        });    
     }
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        displayImages(response);
-    })
+  
 
 /* 
 Pseudocode:
