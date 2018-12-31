96c871b7ba5a4ed3abd13005d2fa3c48b3e7a774
Sarah Kinneer <kinneers@gmail.com>
2018-12-31 00:30:44 -0500

    fixed glitch with remove button in wrong place when favorite gif is unfavorited and returned

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -129,5 +129,8 @@ $(document).ready(function() {
         $('#results').append(removeFavorite); //appends the unfavorited gif back to the results section
         $('#results .addFavorite').show(); //shows the heart button in the returned results
         $('.sidebar .addFavorite').hide();  //keeps the other heart buttons hidden
+        $('.more').hide(); //Hides the previous Add More button
+        $('.grid').append(`<button class='more'>Get More!</button>`); //Appends button to retrieve 10 more gifs on this topic to the end of the div
+
     })
 })
