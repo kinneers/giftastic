a1647c5aa7412b7f32913e0635f14e2506c7b563
Sarah Kinneer <kinneers@gmail.com>
2018-12-30 22:12:23 -0500

    Added button to add more gifs to the page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var topics = ["Brad Pitt", "Angelina Jolie", "Jennifer Lawrence", "Tom Hanks", "Robert De Niro", "Johnny Depp", "Al Pacino", "Denzel Washington", "Russell Crowe", "Leonardo DiCaprio", "Tom Cruise", "John Travolta", "Arnold Schwarzenegger", "Kate Winslet", "Christian Bale", "Morgan Freeman", "Keanu Reeves", "Nicolas Cage", "Hugh Jackman", "Bruce Willis", "Will Smith", "Keira Knightly", "Vin Diesel", "Matt Damon", "Catherine Zeta-Jones", "George Clooney", "Scarlett Johansson", "Robert Downey, Jr.", "Sandra Bullock", "Meg Ryan", "Megan Fox", "Nicole Kidman", "Cameron Diaz", "Katherine Heigl"];
     var btns = 0;
     var selection='';
+    var topic;
+    var offset = 10;
 
     //Adds new user selection to array
     $("#select-topic").on('click', function(event) {
@@ -50,7 +52,7 @@ $(document).ready(function() {
 
     //ajax call to Giphy
     $(document).on('click', '.topic', function(){
-        var topic = $(this).attr('value');
+        topic = $(this).attr('value');
         $.ajax({
             url: "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=HauhqwQL2R2AM9YsD534mHau5NQBTYe7&limit=10",
             method: "GET"
@@ -63,9 +65,10 @@ $(document).ready(function() {
 
     //displays each gif retrieved in the results section
     function displayImages(response) {
-        $('.grid').empty();
-        $('.clickPrompt').show();
+        $('.grid').empty(); //clears previous content
+        $('.clickPrompt').show(); //Shows prompt to click the image to animate
         response.data.forEach(function(image) {
+            //Checks that gif is appropriate for audience prior to adding to page
             if (image.rating !== "r" && image.rating !== "pg-13") {
                 var newDiv = $("<div>");
                 var newImage = $("<img>").attr("class", "gif grid-item").attr("data-state", "still").attr("src", image.images.fixed_width_still.url).attr("data-animate", image.images.fixed_width.url).attr("data-still", image.images.fixed_width_still.url);
@@ -75,13 +78,36 @@ $(document).ready(function() {
                 var favoriteButton = $(`<button class=addFavorite>&#10084;</button>`)
                 newDiv.append(newImage).append(imageTitle).append(imageRating).append(imageImportDateTime).append(favoriteButton);
                 $('#results').append(newDiv);
-                console.log(response);
-                console.log(imageRating);
             }
-        });   
+        });
+        $('.grid').append(`<button class='more'>Get 10 More!</button>`); //Appends button to retrieve 10 more gifs on this topic
+        $(document).on('click', '.more', function(){
+            $.ajax({
+                url: "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=HauhqwQL2R2AM9YsD534mHau5NQBTYe7&limit=10&offset=" + offset,
+                method: "GET"
+            }).then(function(response){
+                response.data.forEach(function(image) {
+                    //Checks that gif is appropriate for audience prior to adding to page
+                    //This code is not DRY, but this is because the event listener is now on the add more button instead of the topic button
+                    if (image.rating !== "r" && image.rating !== "pg-13") {
+                        var newDiv = $("<div>");
+                        var newImage = $("<img>").attr("class", "gif grid-item").attr("data-state", "still").attr("src", image.images.fixed_width_still.url).attr("data-animate", image.images.fixed_width.url).attr("data-still", image.images.fixed_width_still.url);
+                        var imageRating = $('<p>').text("Rating: " + image.rating.toUpperCase());
+                        var imageTitle = $('<p>').text("Title: " + image.title.toUpperCase());
+                        var imageImportDateTime =$('<p>').text("Imported: " + image.import_datetime);
+                        var favoriteButton = $(`<button class=addFavorite>&#10084;</button>`)
+                        newDiv.append(newImage).append(imageTitle).append(imageRating).append(imageImportDateTime).append(favoriteButton);
+                        $('#results').append(newDiv);
+                    }
+                });
+                $('.more').hide(); //Hides the previous Add More button
+                $('.grid').append(`<button class='more'>Get 10 More!</button>`); //Appends button to retrieve 10 more gifs on this topic to the end of the div
+                offset += 10; //Increments the offset to collect the next 10 gifs on the same topic
+            })
+        })
     } 
     
-    //Click to switch between animated and still
+    //Function to animate or unanimate gifs on click (or tap)
     $(document).on("click tap", ".gif", function() {
         var state = $(this).attr("data-state");
         if (state === "still") {
@@ -94,7 +120,7 @@ $(document).ready(function() {
         }
     });
 
-    //Move favorite gifs to favorites area on click of heart button
+    //Moves favorited gifs to favorites area on click of heart button
     $(document).on('click tap','.addFavorite', function(event){
         event.preventDefault();
         var saveFavorite = $(this).parent(); //grabs the div associated with clicked button
