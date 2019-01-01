44697de49386927ea3145d1fe0dda662c1ddcace
Sarah Kinneer <kinneers@gmail.com>
2019-01-01 14:57:03 -0500

    Refactored some lines for readability

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    window.localStorage.clear()
     $('.prompt').hide();
     $('.clickPrompt').hide();
     var topics = ["Brad Pitt", "Angelina Jolie", "Jennifer Lawrence", "Tom Hanks", "Robert De Niro", "Johnny Depp", "Al Pacino", "Denzel Washington", "Russell Crowe", "Leonardo DiCaprio", "Tom Cruise", "John Travolta", "Arnold Schwarzenegger", "Kate Winslet", "Christian Bale", "Morgan Freeman", "Keanu Reeves", "Nicolas Cage", "Hugh Jackman", "Bruce Willis", "Will Smith", "Keira Knightly", "Vin Diesel", "Matt Damon", "Catherine Zeta-Jones", "George Clooney", "Scarlett Johansson", "Robert Downey, Jr.", "Sandra Bullock", "Meg Ryan", "Megan Fox", "Nicole Kidman", "Cameron Diaz", "Katherine Heigl"];
@@ -11,12 +12,21 @@ $(document).ready(function() {
         //Checks that gif is appropriate for audience prior to adding to page (this is called in the API call now and could be refactored, but I left it as an extra precaution)
         if (image.rating !== "r" && image.rating !== "pg-13") {
             var newDiv = $("<div>");
-            var newImage = $("<img>").attr("class", "gif grid-item").attr("data-state", "still").attr("src", image.images.fixed_width_still.url).attr("data-animate", image.images.fixed_width.url).attr("data-still", image.images.fixed_width_still.url);
+            var newImage = $("<img>")
+                        .attr("class", "gif grid-item")
+                        .attr("data-state", "still")
+                        .attr("src", image.images.fixed_width_still.url)
+                        .attr("data-animate", image.images.fixed_width.url)
+                        .attr("data-still", image.images.fixed_width_still.url);
             var imageRating = $('<p class="itemContent">').text("Rating: " + image.rating.toUpperCase());
             var imageTitle = $('<p class="itemContent">').text("Title: " + image.title.toUpperCase());
             var imageImportDateTime =$('<p class="itemContent">').text("Imported: " + image.import_datetime);
             var favoriteButton = $(`<button class=addFavorite>&#10084;</button>`)
-            newDiv.append(newImage).append(imageTitle).append(imageRating).append(imageImportDateTime).append(favoriteButton);
+            newDiv.append(newImage)
+                  .append(imageTitle)
+                  .append(imageRating)
+                  .append(imageImportDateTime)
+                  .append(favoriteButton);
             $('#results').append(newDiv);
         }
     }
@@ -80,7 +90,7 @@ $(document).ready(function() {
         $('.grid').empty(); //clears previous content
         $('.clickPrompt').show(); //Shows prompt to click the image to animate
         response.data.forEach(function(image) {
-            populateDiv(image); //Appends 10 images (fewer if some are inappropriately rated)
+            populateDiv(image); //Appends 10 appropriate images
         });
         $('.grid').append(`<button class='more'>Get More!</button>`); //Appends button to retrieve 10 more gifs on this topic
         
@@ -101,7 +111,7 @@ $(document).ready(function() {
         })
     } 
     
-    //Function to animate or unanimate gifs on click (or tap)
+    //Function to switch between animated or still on click (or tap)
     $(document).on("click tap", ".gif", function(event) {
         event.preventDefault();
         var state = $(this).attr("data-state");
@@ -132,8 +142,7 @@ $(document).ready(function() {
         $('#results').append(removeFavorite); //appends the unfavorited gif back to the results section
         $('#results .addFavorite').show(); //shows the heart button in the returned results
         $('.sidebar .addFavorite').hide();  //keeps the other heart buttons hidden
-                        $('.more').hide(); //Hides the previous Add More button
-                $('.grid').append(`<button class='more'>Get More!</button>`); //Appends button to retrieve 10 more gifs on this topic to the end of the div
-
+        $('.more').hide(); //Hides the previous Add More button
+        $('.grid').append(`<button class='more'>Get More!</button>`); //Appends button to retrieve 10 more gifs on this topic to the end of the div
     })
 })
